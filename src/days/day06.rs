9f4c716849f5202f19f0edb4d27e78096ede7b82
NEUDitao <edward.d.li@gmail.com>
2023-12-06 00:14:54 -0500

    day 6

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -5,22 +5,55 @@ use std::{collections::HashSet, fs::read_to_string};
 
 pub fn solve() -> SolutionPair {
     let input = read_to_string("input/day06").unwrap();
-    let first_row = input.split('\n').next().unwrap();
+    let mut rows = input.split('\n');
+    let mut time = rows.next().unwrap().split_whitespace();
+    let mut distance = rows.next().unwrap().split_whitespace();
 
-    let mut x = 0;
-    for idx in 14..(first_row.len()) {
-        let seq = &first_row[idx - 14..idx];
-        let seq_len = seq.len();
-        let uniq: HashSet<char> = HashSet::from_iter(seq.chars());
-        if uniq.len() == seq_len {
-            x = idx;
-            break;
-        }
-    }
+    time.next();
+    distance.next();
+
+    let zip = time.zip(distance);
+    let res = zip
+        .clone()
+        .map(|(time, distance)| {
+            let max_time = time.parse::<u64>().unwrap();
+            let distance_to_beat = distance.parse::<u64>().unwrap();
+            (1..max_time)
+                .map(|n| {
+                    if n * (max_time - n) > distance_to_beat {
+                        1
+                    } else {
+                        0
+                    }
+                })
+                .sum::<usize>()
+        })
+        .fold(1, |acc, e| acc * e);
+
+    let (time, distance) = zip.fold(
+        ("".to_owned(), "".to_owned()),
+        |(acc_time, acc_distance), (time, distance)| {
+            (
+                format!("{}{}", acc_time, time),
+                format!("{}{}", acc_distance, distance),
+            )
+        },
+    );
+    let max_time = time.parse::<usize>().unwrap();
+    let distance_to_beat = distance.parse::<usize>().unwrap();
+    let res_2 = (1..max_time)
+        .map(|n| {
+            if n * (max_time - n) > distance_to_beat {
+                1
+            } else {
+                0
+            }
+        })
+        .sum::<usize>();
 
     // Your solution here...
-    let sol1: usize = x;
-    let sol2: u64 = 0;
+    let sol1: usize = res;
+    let sol2: usize = res_2;
 
     (Solution::from(sol1), Solution::from(sol2))
 }
