8f1cc3dda5f8bf3a5697ae339f419b75cd63f8a1
NEUDitao <edward.d.li@gmail.com>
2023-12-08 00:34:47 -0500

    day 8

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -3,134 +3,144 @@ use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-fn get_if_visible(map: &Vec<Vec<u32>>, row: usize, col: usize) -> bool {
-    let cur_val = map[row][col];
-
-    row == 0
-        || col == 0
-        || row == map.len() - 1
-        || col == map[row as usize].len() - 1
-        || (cur_val > map[row][col - 1] && get_if_visible_left(map, row, col - 1, cur_val))
-        || (cur_val > map[row][col + 1] && get_if_visible_right(map, row, col + 1, cur_val))
-        || (cur_val > map[row + 1][col] && get_if_visible_down(map, row + 1, col, cur_val))
-        || (cur_val > map[row - 1][col] && get_if_visible_up(map, row - 1, col, cur_val))
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right,
 }
 
-fn get_if_visible_left(map: &Vec<Vec<u32>>, row: usize, col: usize, cur_val: u32) -> bool {
-    col == 0 || (cur_val > map[row][col - 1] && get_if_visible_left(map, row, col - 1, cur_val))
-}
-
-fn get_if_visible_right(map: &Vec<Vec<u32>>, row: usize, col: usize, cur_val: u32) -> bool {
-    col == map.len() - 1
-        || (cur_val > map[row][col + 1] && get_if_visible_right(map, row, col + 1, cur_val))
-}
-
-fn get_if_visible_up(map: &Vec<Vec<u32>>, row: usize, col: usize, cur_val: u32) -> bool {
-    row == 0 || (cur_val > map[row - 1][col] && get_if_visible_up(map, row - 1, col, cur_val))
-}
-
-fn get_if_visible_down(map: &Vec<Vec<u32>>, row: usize, col: usize, cur_val: u32) -> bool {
-    row == map.len() - 1
-        || (cur_val > map[row + 1][col] && get_if_visible_down(map, row + 1, col, cur_val))
-}
-
-fn get_val(map: &Vec<Vec<u32>>, row: usize, col: usize) -> i32 {
-    let cur_val = map[row][col];
-    // let mut tot = 1;
-
-    if col == 0 || col == map.len() - 1 || row == map.len() - 1 || row == 0 {
-        0
-    } else {
-        get_val_left(map, row, col - 1, cur_val)
-            * get_val_right(map, row, col + 1, cur_val)
-            * get_val_down(map, row + 1, col, cur_val)
-            * get_val_up(map, row - 1, col, cur_val)
+fn go_dir(row: String, dir: &Direction) -> String {
+    let row = row.split_whitespace().collect::<Vec<_>>();
+    let left = &row.get(2).unwrap()[1..4];
+    let right = &row.get(3).unwrap()[0..3];
+    match dir {
+        Direction::Left => left,
+        Direction::Right => right,
     }
+    .to_owned()
 }
 
-fn get_val_left(map: &Vec<Vec<u32>>, row: usize, col: usize, cur_val: u32) -> i32 {
-    if col == 0 {
-        1
-    } else if cur_val <= map[row][col] {
-        1
-    } else {
-        1 + get_val_left(map, row, col - 1, cur_val)
-    }
+fn get_row(rows: &Vec<String>, row_to_find: &str) -> String {
+    rows.iter()
+        .find(|row| row.starts_with(row_to_find))
+        .unwrap()
+        .to_owned()
 }
 
-fn get_val_right(map: &Vec<Vec<u32>>, row: usize, col: usize, cur_val: u32) -> i32 {
-    if col == map.len() - 1 {
-        1
-    } else if cur_val <= map[row][col] {
-        1
-    } else {
-        1 + get_val_right(map, row, col + 1, cur_val)
-    }
+fn get_loc(row: &str) -> String {
+    row.split_whitespace().next().unwrap().to_owned()
 }
 
-fn get_val_up(map: &Vec<Vec<u32>>, row: usize, col: usize, cur_val: u32) -> i32 {
-    if row == 0 {
-        1
-    } else if cur_val <= map[row][col] {
-        1
-    } else {
-        1 + get_val_up(map, row - 1, col, cur_val)
-    }
+fn lcm(a: u128, b: u128) -> u128 {
+    a * b / gcd(a, b)
 }
 
-fn get_val_down(map: &Vec<Vec<u32>>, row: usize, col: usize, cur_val: u32) -> i32 {
-    if row == map.len() - 1 {
-        1
-    } else if cur_val <= map[row][col] {
-        1
+fn gcd(a: u128, b: u128) -> u128 {
+    if b == 0 {
+        a
     } else {
-        1 + get_val_down(map, row + 1, col, cur_val)
+        gcd(b, a % b)
     }
 }
 
 pub fn solve() -> SolutionPair {
     let input = read_to_string("input/day08").unwrap();
-    let rows = input.split('\n');
+    let mut rows = input.split('\n');
 
-    let mut map: Vec<Vec<u32>> = Vec::new();
-    for row in rows {
-        let mut map_row: Vec<u32> = Vec::new();
-        for char in row.chars() {
-            map_row.push(char.to_digit(10).unwrap());
-        }
-        map.push(map_row);
-    }
+    let steps = rows
+        .next()
+        .unwrap()
+        .chars()
+        .map(|c| {
+            if c == 'R' {
+                Direction::Right
+            } else {
+                Direction::Left
+            }
+        })
+        .collect::<Vec<_>>();
+    rows.next();
+    let rows = rows.map(|s| s.to_owned()).collect::<Vec<_>>();
 
+    let mut current_loc = "AAA".to_owned();
     let mut count = 0;
-    for row in 0..map.len() {
-        for col in 0..map[row].len() {
-            let is_visible = get_if_visible(&map, row, col);
-            if is_visible {
-                count += 1;
-            }
+    let mut index_in_steps = 0;
+
+    loop {
+        if index_in_steps >= steps.len() {
+            index_in_steps = 0;
+        }
+        let curr_dir = steps.get(index_in_steps).unwrap();
+        let curr_row = get_row(&rows, &current_loc);
+        let loc = go_dir(curr_row, curr_dir);
+
+        current_loc = loc;
+        index_in_steps += 1;
+
+        count += 1;
+
+        if current_loc == "ZZZ" {
+            break;
         }
     }
 
-    let mut max = 0;
-
-    for row in 0..map.len() {
-        for col in 0..map[row].len() {
-            let val = get_val(&map, row, col);
-            if val > max {
-                max = val;
-            }
-        }
-    }
-
-    // dbg!(get_val(&map, 3, 2));
-    // dbg!(get_val_left(&map, 3, 2, 3));
-    // dbg!(get_val_right(&map, 3, 2, 3));
-    // dbg!(get_val_up(&map, 3, 2, 3));
-    // dbg!(get_val_down(&map, 3, 2, 3));
-
-    // Your solution here...
+    // // Your solution here...
     let sol1: u64 = count;
-    let sol2 = max;
+
+    let mut current_locs = rows
+        .clone()
+        .into_iter()
+        .filter_map(|row| {
+            let loc = get_loc(&row);
+            if loc.ends_with("A") {
+                Some(loc.to_owned())
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    let mut count = 0;
+    let mut index_in_steps = 0;
+    let mut steps_required = Vec::new();
+
+    loop {
+        if index_in_steps >= steps.len() {
+            index_in_steps = 0;
+        }
+        let curr_dir = steps.get(index_in_steps).unwrap();
+
+        current_locs = current_locs
+            .iter()
+            .map(|loc| {
+                let curr_row = get_row(&rows, &loc);
+                go_dir(curr_row, curr_dir)
+            })
+            .collect::<Vec<_>>();
+
+        index_in_steps += 1;
+        count += 1;
+
+        for loc in &current_locs {
+            if loc.ends_with("Z") {
+                steps_required.push(count);
+            }
+        }
+        current_locs = current_locs
+            .iter()
+            .filter(|loc| !loc.ends_with("Z"))
+            .map(|s| s.to_owned())
+            .collect::<Vec<_>>();
+
+        if count % 100000 == 0 {
+            println!("here!");
+        }
+
+        if current_locs.is_empty() {
+            break;
+        }
+    }
+
+    let sol2 = steps_required.iter().fold(1, |acc, x| lcm(acc, *x));
 
     (Solution::from(sol1), Solution::from(sol2))
 }
