3b7c740fcffa02959274e3cf532c18bec2afae22
NEUDitao <edward.d.li@gmail.com>
2023-12-09 00:16:39 -0500

    day 9

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -6,66 +6,76 @@ use std::{
 
 ///////////////////////////////////////////////////////////////////////////////
 
-fn figure_dir(dir: &str) -> (i32, i32) {
-    match dir {
-        "R" => (1, 0),
-        "U" => (0, 1),
-        "L" => (-1, 0),
-        "D" => (0, -1),
-        _ => panic!("fuck"),
+fn calculate_diffs(row: Vec<i64>) -> Vec<i64> {
+    let mut res = Vec::new();
+    for i in 0..(row.len() - 1) {
+        let a = row.get(i).unwrap();
+        let b = row.get(i + 1).unwrap();
+        res.push(b - a);
     }
+    res
 }
 
-fn update(head: (i32, i32), tail: (i32, i32)) -> (i32, i32) {
-    match (head.0 - tail.0, head.1 - tail.1) {
-        (2, 0) => (tail.0 + 1, tail.1),
-        (-2, 0) => (tail.0 - 1, tail.1),
-        (0, 2) => (tail.0, tail.1 + 1),
-        (0, -2) => (tail.0, tail.1 - 1),
-        (2, 1) | (1, 2) | (2, 2) => (tail.0 + 1, tail.1 + 1),
-        (2, -1) | (1, -2) | (2, -2) => (tail.0 + 1, tail.1 - 1),
-        (-2, 1) | (-1, 2) | (-2, 2) => (tail.0 - 1, tail.1 + 1),
-        (-2, -1) | (-1, -2) | (-2, -2) => (tail.0 - 1, tail.1 - 1),
-        _ => tail,
+fn get_extrapolated_value(row: String) -> i64 {
+    let mut row = row
+        .split_whitespace()
+        .map(|n| n.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut diffs_list = Vec::new();
+    diffs_list.push(row.clone());
+    loop {
+        let diffs = calculate_diffs(row);
+        if diffs.iter().all(|d| d == &0) {
+            break;
+        }
+        diffs_list.push(diffs.clone());
+        row = diffs;
     }
+
+    let mut end = 0;
+    while let Some(mut diff_list) = diffs_list.pop() {
+        end = end + diff_list.pop().unwrap();
+    }
+
+    end
+}
+
+fn get_extrapolated_value_rev(row: String) -> i64 {
+    let mut row = row
+        .split_whitespace()
+        .map(|n| n.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut diffs_list = Vec::new();
+    diffs_list.push(row.clone());
+    loop {
+        let diffs = calculate_diffs(row);
+        if diffs.iter().all(|d| d == &0) {
+            break;
+        }
+        diffs_list.push(diffs.clone());
+        row = diffs;
+    }
+
+    let mut beg = 0;
+    while let Some(diff_list) = diffs_list.pop() {
+        beg = diff_list.get(0).unwrap() - beg;
+    }
+
+    beg
 }
 
 pub fn solve() -> SolutionPair {
     let input = read_to_string("input/day09").unwrap();
-
-    let mut rope_parts: BTreeMap<i32, (i32, i32)> = BTreeMap::new();
-
-    for i in 0..10 {
-        rope_parts.insert(i, (0, 0));
-    }
-
-    let mut seen: BTreeSet<(i32, i32)> = BTreeSet::new();
-
-    for line in input.lines() {
-        let mut line_as_iter = line.split_ascii_whitespace();
-        let dir = figure_dir(line_as_iter.next().unwrap());
-        let num = str::parse::<i32>(line_as_iter.next().unwrap()).unwrap();
-
-        for _n in 0..num {
-            let first_head = rope_parts[&0];
-            rope_parts.insert(0, (first_head.0 + dir.0, first_head.1 + dir.1));
-
-            for rope_section in 1..10 {
-                let head = rope_parts[&(rope_section - 1)];
-                let tail = rope_parts[&rope_section];
-                let new_tail = update(head, tail);
-                rope_parts.insert(rope_section, new_tail);
-
-                if rope_section == 9 {
-                    seen.insert(tail);
-                }
-            }
-        }
-    }
+    let rows = input.split("\n");
 
     // Your solution here...
-    let sol1 = seen.len();
-    let sol2 = 0;
+    let sol1: i64 = rows
+        .clone()
+        .map(|r| get_extrapolated_value(r.to_owned()))
+        .sum();
+    let sol2: i64 = rows.map(|r| get_extrapolated_value_rev(r.to_owned())).sum();
 
     (Solution::from(sol1), Solution::from(sol2))
 }
